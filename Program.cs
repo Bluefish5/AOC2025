0e1fba8508ce30a2b1eee44e555d9a1d7cf81d50
Rafal <60304472+Bluefish5@users.noreply.github.com>
2025-12-04 19:03:19 +0100

    Day 4 completed

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ class Program
     static void Main(string[] args)
     {
 
-        var NAME_PROGRAM_TO_RUN = "2025_3_p2";
+        var NAME_PROGRAM_TO_RUN = "2025_4_p2";
 
         System.Console.WriteLine("Running program: " + NAME_PROGRAM_TO_RUN);
 
@@ -39,6 +39,15 @@ class Program
             case "2025_3_p2":
                 runner.program_2025_3_p2();
                 break;
+
+            case "2025_4_p1":
+                runner.program_2025_4_p1();
+                break;
+
+            case "2025_4_p2":
+                runner.program_2025_4_p2();
+                break;
+
 
             default:
                 Console.WriteLine("Set up var to execute: NAME_PROGRAM_TO_RUN.");
@@ -277,6 +286,122 @@ class ProgramRunner
         }
 
         Console.WriteLine("Sum of all batteries: " + sum);
+    }
+    public void program_2025_4_p1()
+    {
+        long sumRollsOfPaper = 0;
+        string path = "files\\data_2025_4.txt";
+
+        var lines = File.ReadAllLines(path);
+
+        int paperAround = 0;
+
+        var matrix = new List<(int dx, int dy)>
+        {
+            (-1, -1),
+            (-1,  0),
+            (-1,  1),
+            ( 0, -1),
+            ( 0,  1),
+            ( 1, -1),
+            ( 1,  0),
+            ( 1,  1)
+        };
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            for (int j = 0; j < lines[i].Length; j++)
+            {
+                paperAround = 0;
+                if (lines[i][j] == '@')
+                {
+                    foreach (var (dx, dy) in matrix)
+                    {
+                        int newX = i + dx;
+                        int newY = j + dy;
+                        if (newX >= 0 && newX < lines.Length && newY >= 0 && newY < lines[i].Length)
+                        {
+                            if (lines[newX][newY] == '@')
+                            {
+                                paperAround++;
+                            }
+                        }
+                    }
+                    if (paperAround < 4) sumRollsOfPaper++;
+
+                }
+            }
+        }
+        System.Console.WriteLine("Sum of all  rolls of paper that can be accessed by a forklift: " + sumRollsOfPaper);
+    }
+
+    public void program_2025_4_p2()
+    {
+        long totalPapers = 0;
+        string path = "files\\data_2025_4.txt";
+
+        var lines = File.ReadAllLines(path)
+                   .Select(x => x.ToCharArray())
+                   .ToArray();
+
+        int paperAround = 0;
+        bool paperRunOut = false;
+
+        var matrix = new List<(int dx, int dy)>
+        {
+            (-1, -1),
+            (-1,  0),
+            (-1,  1),
+            ( 0, -1),
+            ( 0,  1),
+            ( 1, -1),
+            ( 1,  0),
+            ( 1,  1)
+        };
+
+        while (!paperRunOut)
+        {
+            var toRemove = new List<(int x, int y)>();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    paperAround = 0;
+                    if (lines[i][j] == '@')
+                    {
+                        int newX = 0;
+                        int newY = 0;
+                        foreach (var (dx, dy) in matrix)
+                        {
+                            newX = i + dx;
+                            newY = j + dy;
+
+
+                            if (newX >= 0 && newX < lines.Length && newY >= 0 && newY < lines[i].Length)
+                            {
+                                if (lines[newX][newY] == '@') paperAround++;
+                               
+                            }
+                        }
+                        if (paperAround < 4) toRemove.Add((i, j));
+                        
+
+                    }
+                }
+            }
+            if (toRemove.Count == 0) paperRunOut = true;
+
+
+            foreach (var (x, y) in toRemove)
+            {
+                lines[x][y] = '.';
+            }
+
+            totalPapers += toRemove.Count;
+        }
+        
+        System.Console.WriteLine("Sum of all  rolls of paper that can be accessed by a forklift: " + totalPapers);
     }
 
 
