eb523c10ef0fda2aa904487b62789ac45e25f22d
Rafal <60304472+Bluefish5@users.noreply.github.com>
2025-12-06 18:19:48 +0100

    Day 6 complete

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.Metrics;
+using static System.Net.Mime.MediaTypeNames;
 
 
 class Program
@@ -8,7 +9,7 @@ class Program
     static void Main(string[] args)
     {
 
-        var NAME_PROGRAM_TO_RUN = "2025_5_p2";
+        var NAME_PROGRAM_TO_RUN = "2025_6_p2";
 
         System.Console.WriteLine("Running program: " + NAME_PROGRAM_TO_RUN);
 
@@ -56,6 +57,13 @@ class Program
                 runner.program_2025_5_p2();
                 break;
 
+            case "2025_6_p1":
+                runner.program_2025_6_p1();
+                break;
+
+            case "2025_6_p2":
+                runner.program_2025_6_p2();
+                break;
 
             default:
                 Console.WriteLine("Set up var to execute: NAME_PROGRAM_TO_RUN.");
@@ -497,7 +505,126 @@ class ProgramRunner
 
         Console.WriteLine("Total fresh IDs: " + total);
     }
+    public void program_2025_6_p1()
+    {
+        string path = "files\\data_2025_6.txt";
+        var lines = File.ReadAllLines(path);
 
+        List<List<long>> numbersInColumns = new List<List<long>>();
+        List<string> operators = new List<string>();
+
+        long totalSum = 0;
+        long tmpSum = 0;
+
+        for (int i = 0; i < 4; i++)
+        {
+            List<long> numbers = lines[i]
+                .Split(new[] { ' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse)
+                .ToList();
+            
+            numbersInColumns.Add(numbers);
+        }
+        operators = lines[4]
+        .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+        .ToList();
+
+        for(int i = 0; i < numbersInColumns[0].Count; i++)
+        {
+            for(int j = 0; j<4; j++)
+            {
+                if (operators[i] == "+")
+                {
+                    tmpSum += numbersInColumns[j][i];
+                }
+                else if (operators[i] == "*")
+                {
+                    if (j == 0) tmpSum = numbersInColumns[j][i];
+                    else tmpSum *= numbersInColumns[j][i];
+                }
+            }
+            totalSum += tmpSum;
+            tmpSum = 0;
+        }
+        Console.WriteLine("Total sum: " + totalSum);
+    }
+    public void program_2025_6_p2()
+    {
+        string path = "files\\data_2025_6.txt";
+        var lines = File.ReadAllLines(path);
+
+        List<string> operators = new List<string>();
+
+        double totalSum = 0;
+
+        string tmpStringNumber = "";
+
+        int cursorOperator = 0;
+
+        List<int> numbers = new List<int>();
+
+        operators = lines[4]
+        .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+        .ToList();
+
+        operators.Reverse();
+
+        for (int i = lines[0].Length - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                tmpStringNumber += lines[j][i];
+            }
+            if (!tmpStringNumber.All(c => c == ' '))
+            {
+                numbers.Add(int.Parse(tmpStringNumber.Trim()));
+            }
+            else 
+            {
+                if (operators[cursorOperator] == "+")
+                {
+                    foreach (var number in numbers)
+                    {
+                        totalSum += number;
+                    }
+                }
+                else if (operators[cursorOperator] == "*")
+                {
+                    double tmpProduct = 1;
+                    foreach (var number in numbers)
+                    {
+                        tmpProduct *= number;
+                    }
+                    totalSum += tmpProduct;
+                }
+                cursorOperator++;
+                numbers.Clear();
+            }
+
+            tmpStringNumber = "";
+        }
+        if (numbers.Count > 0)
+        {
+            if (operators[cursorOperator] == "+")
+            {
+                foreach (var number in numbers)
+                {
+                    totalSum += number;
+                }
+            }
+            else if (operators[cursorOperator] == "*")
+            {
+                double tmpProduct = 1;
+                foreach (var number in numbers)
+                {
+                    tmpProduct *= number;
+                }
+                totalSum += tmpProduct;
+            }
+        }
+        Console.WriteLine("Total sum: " + totalSum);
+    }
+   
 }
 
 
