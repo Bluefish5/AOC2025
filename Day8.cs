49e709d7a5d0c67e4e71f4087387393b86e975f7
Rafal <60304472+Bluefish5@users.noreply.github.com>
2025-12-11 08:42:18 +0100

    Day 8 digging

---
+++ Day8.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2025
+{
+    public static class Day8
+    {
+
+        public static void Execute()
+        {
+            Console.WriteLine("Day 8 part 1 execution started.");
+
+
+
+            /*
+             * var points = loadPoints();
+             * 
+             * create all possible connection between points with their distances
+             * 
+             * find 1000 shortest distances 
+             * 
+             * create circout for each point 
+             * 
+             * for each distnace check if circout is connected and unified circout together
+             * 
+             */
+
+
+        }
+    }
+}
